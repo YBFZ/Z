61f357c1ba7a4f61750354f310b2d4e6b8847229
YBFZ <90503651+YBFZ@users.noreply.github.com>
2021-09-15 13:57:28 +0800

    Add files via upload

---
+++ springboot-01-halloworld/src/main/java/com/z/Springboot01HalloworldApplication.java
@@ -0,0 +1,13 @@
+package com.z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01HalloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01HalloworldApplication.class, args);
+    }
+
+}
---
+++ springboot-01-halloworld/src/test/java/com/z/Springboot01HalloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01HalloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
